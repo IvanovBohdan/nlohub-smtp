11ae17824789abfe08b23e81b130de00cb687815
IvanovBohdan <83204262+IvanovBohdan@users.noreply.github.com>
2025-03-19 07:41:05 +0000

    FIX: expiration of HSET fields now works correctly

--- services/publishService.js
+++ services/publishService.js
@@ -6,10 +6,12 @@ export async function publishEmail(email) {
     try {
         const redisKey = `${email.to.text.replaceAll(/\s/g, ":")}:${uuidv4()}`;
         for (const recipient of email.to.value) {
-            await redisClient.hSet(recipient.address, redisKey, JSON.stringify(email), {
-                EX: 60 * 60 * 24,
-            });
-            await redisClient.publish(recipient.address, redisKey);
+            await redisClient
+                .multi()
+                .hSet(recipient.address, redisKey, JSON.stringify(email))
+                .hExpire(recipient.address, redisKey, 60 * 60 * 24)
+                .publish(recipient.address, redisKey)
+                .exec();
         }
     } catch (error) {
         console.error("Email processing failed", error);
